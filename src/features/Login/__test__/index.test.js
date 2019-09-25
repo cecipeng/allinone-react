dc0b4ad706a8b0517136e3e94c3839945c2958e8
Ferris <bin@edmodo.com>
2019-09-25 18:37:24 +0800

    【v0.1】临时提交

---
+++ src/features/Login/__test__/index.test.js
@@ -0,0 +1,14 @@
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json';
+
+// ====== Component ====== //
+import { Login } from '../components/container'
+
+describe('features -> Login -> container', () => {
+  it('renders as expected', () => {
+    const tree = shallow(
+      <Login />
+    );
+    expect(toJson(tree)).toMatchSnapshot()
+  })
+})
